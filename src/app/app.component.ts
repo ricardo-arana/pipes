cdf9c16857ffcfa9f79d22cd0a806903bdbeb03b
Ricardo Arana Reyes <raranare@everis.com>
2020-06-08 18:40:59 -0500

    v

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  
+    nombre    : string   = 'Capitán América';
+    nombre2   : string   = 'ferNanDo hErrerA jImeneZ';
+    personajes: string[] = ['Ironman', 'Spiderman', 'Thor','Loki','Groot'];
+    PI        : number   = Math.PI;
+    porcentaje: number   = 0.235;
+    salario   : number   = 1234.5
+    fecha     : Date     = new Date();
+    activar   : boolean  = true;
+
+    idioma    : string = 'en';
+    videoUrl  : string = 'https://www.youtube.com/embed/-CBKW-90VxQ';
+
+    valorPromesa = new Promise<string>( (resolve) => {
+
+      setTimeout(() => {
+        resolve('llego la data');
+      }, 4500);
+
+    });
+
+    heroe = {
+      nombre: 'Logan',
+      clave: 'Wolverine',
+      edad: 500,
+      direccion: {
+        calle: 'Primera',
+        casa: 20
+      }
+    }
+
 }
